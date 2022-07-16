e37620dfa8635a92748caac4ed1386c809816b4b
Code4Daze <jevon.fillette@gmail.com>
2022-07-16 13:53:44 -0400

    Loop through the gameArray to display on board

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const gameBoard = (() => {
-  
+  let gameArray = ["X", "O", "X", "O", "X", "O","X", "O", "X"];
+  gameArray.forEach((marker, index) => {
+    let currentSpace = document.getElementById(`${index}`);
+    currentSpace.appendChild(document.createTextNode(`${marker}`));
+  })
 })();
