95d1934d9830f98ac0e2f3e976bef1a8270fccbb
Code4Daze <jevon.fillette@gmail.com>
2022-07-17 01:11:46 -0400

    Change marker (X|O) based on playerTurn

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const createPlayer = (name, marker) => {
+const createPlayer = (name, marker, turn) => {
   getName = () => name;
   getMarker = () => marker;
-  return {getName, getMarker};
+  return {getName, getMarker, turn};
 };
 
 const gameBoard = (() => {
   let gameArray = ["", "", "", "", "", "", "", "", ""];
+
   function displayMarks(array) {
     for (let i = 0; i < array.length; i++) {
       let currentSpace = document.getElementById(`${i}`);
@@ -16,12 +17,31 @@ const gameBoard = (() => {
 })();
 
 const gameFlow = (() => {
-  let player1 = createPlayer("player1", "X");
-  let player2 = createPlayer("player2", "O");
+  let player1 = createPlayer("A", "X", true);
+  let player2 = createPlayer("B", "O", false);
+
   let gameSquares = document.querySelectorAll('.gameSquare');
   gameSquares.forEach(square => square.addEventListener('click', addMark));
+
   function addMark() {
-    gameBoard.gameArray.splice(this.id, 1, player1.getMarker());
-    gameBoard.displayMarks(gameBoard.gameArray);
+    if (player1.turn) {
+      if (this.textContent === "") {
+        gameBoard.gameArray.splice(this.id, 1, player1.getMarker());
+        gameBoard.displayMarks(gameBoard.gameArray);
+      } else {
+        return;
+      }
+      player1.turn = false;
+      player2.turn = true;
+    } else {
+      if (this.textContent === "") {
+        gameBoard.gameArray.splice(this.id, 1, player2.getMarker());
+        gameBoard.displayMarks(gameBoard.gameArray);
+      } else {
+        return;
+      }
+      player1.turn = true;
+      player2.turn = false;
+    }
   }
 })();
