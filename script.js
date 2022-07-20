1a2640c45a33b69ddcee011f1b6630ef8e3c47d8
Code4Daze <jevon.fillette@gmail.com>
2022-07-20 17:38:53 -0400

    Add random AI logic

--- script.js
+++ script.js
@@ -8,11 +8,8 @@ const newGame = (() => {
   let playerGame = document.querySelector('.vsPlayer');
   playerGame.addEventListener('click', choosePlayers);
 
-  let gameStart = document.querySelector('.startGame');
-  gameStart.addEventListener('click', showGame);
-
-  let returnBtn = document.querySelector('.returnButton');
-  returnBtn.addEventListener('click', backToStart);
+  let playerGameStart = document.querySelector('.gameStartPlayer');
+  playerGameStart.addEventListener('click', showPlayerGame);
 
   function choosePlayers() {
     let newGameDiv = document.querySelector('.newGameOptions');
@@ -21,7 +18,7 @@ const newGame = (() => {
     vsPlayerDiv.classList.remove('hidden');
   }
 
-  function showGame() {
+  function showPlayerGame() {
     let player1Name = document.querySelector('#player1Name').value;
     let player2Name = document.querySelector('#player2Name').value;
 
@@ -42,14 +39,53 @@ const newGame = (() => {
     let name2 = document.querySelector('.name2');
     name2.textContent = player2Name;
 
-    gameFlow(player1Name, player2Name);
+    playerGameFlow(player1Name, player2Name);
   }
+
+  let AIGame = document.querySelector('.vsAI');
+  AIGame.addEventListener('click', showAISettings);
+
+  let AIGameStart = document.querySelector('.gameStartAI');
+  AIGameStart.addEventListener('click', showAIGame);
+
+  function showAISettings() {
+    let newGameDiv = document.querySelector('.newGameOptions');
+    newGameDiv.classList.add('hidden');
+    let vsAIDiv = document.querySelector('.vsAIOptions');
+    vsAIDiv.classList.remove('hidden');
+  }
+
+  function showAIGame() {
+    let playerName = document.querySelector('#playerName').value;
+    let AIName = "AI";
+
+    if (playerName === "") {
+      playerName = "Player";
+    }
+
+    let vsAIDiv = document.querySelector('.vsAIOptions');
+    vsAIDiv.classList.add('hidden');
+    let gameSpace = document.querySelector('.gameSpace');
+    gameSpace.classList.remove('hidden');
+
+    let name1 = document.querySelector('.name1');
+    name1.textContent = playerName;
+    let name2 = document.querySelector('.name2');
+    name2.textContent = AIName;
+
+    AIGameFlow(playerName, AIName);
+  }
+
+  let returnBtn = document.querySelectorAll('.returnButton');
+  returnBtn.forEach(btn => btn.addEventListener('click', backToStart));
 
   function backToStart() {
     let newGameDiv = document.querySelector('.newGameOptions');
     newGameDiv.classList.remove('hidden');
     let vsPlayerDiv = document.querySelector('.vsPlayerOptions');
     vsPlayerDiv.classList.add('hidden');
+    let vsAIDiv = document.querySelector('.vsAIOptions');
+    vsAIDiv.classList.add('hidden');
   }
 })();
 
@@ -92,7 +128,7 @@ const gameBoard = (() => {
   return {gameArray, displayMarks, checkWinner};
 })();
 
-const gameFlow = (a, b) => {
+const playerGameFlow = (a, b) => {
 
   let player1 = createPlayer(a, "X", true);
   let player2 = createPlayer(b, "O", false);
@@ -157,6 +193,111 @@ const gameFlow = (a, b) => {
       square.classList.add('taken');
     });
   }
+
+  function resetGame() {
+    let winScreen = document.querySelector('.winnerScreen');
+    winScreen.textContent = "";
+    winScreen.classList.add('hidden');
+    gameBoard.gameArray = ["", "", "", "", "", "", "", "", ""];
+    gameBoard.displayMarks(gameBoard.gameArray);
+    gameSquares.forEach(square => {
+      square.addEventListener('click', addMark);
+      square.classList.remove('taken');
+    });
+    player1.turn = true;
+    player2.turn = false;
+    name1.classList.add('turnShadow');
+    name2.classList.remove('turnShadow');
+  }
+
+  function startNewGame() {
+    window.location.reload();
+  }
+};
+
+const AIGameFlow = (a, b) => {
+
+  let player1 = createPlayer(a, "X", true);
+  let player2 = createPlayer(b, "O", false);
+
+  let gameSquares = document.querySelectorAll('.gameSquare');
+  gameSquares.forEach(square => square.addEventListener('click', addMark));
+
+  let name1 = document.querySelector('.name1');
+  let name2 = document.querySelector('.name2');
+
+  function addMark() {
+    if (this.textContent !== "") {
+      return;
+    } else if (player1.turn) {
+      gameBoard.gameArray.splice(this.id, 1, player1.getMarker());
+      this.classList.add('taken');
+      player1.turn = false;
+      player2.turn = true;
+      name1.classList.remove('turnShadow');
+      name2.classList.add('turnShadow');
+      gameBoard.displayMarks(gameBoard.gameArray);
+      displayWinner(gameBoard.checkWinner(gameBoard.gameArray));
+
+      setTimeout(AIMove, 2000);
+    }
+  }
+
+  function AIMove() {
+    let winScreen = document.querySelector('.winnerScreen');
+    let indexes = [];
+      for (let i = 0; i < gameBoard.gameArray.length; i++) {
+        if (gameBoard.gameArray[i] === "") {
+          indexes.push(i);
+        }
+      }
+
+      if (indexes.length > 0 && winScreen.classList.contains('hidden')) {
+        let item = indexes[Math.floor(Math.random()*indexes.length)];
+        gameBoard.gameArray.splice(item, 1, player2.getMarker());
+        let p2GameSquare = document.getElementById(`${item}`);
+        p2GameSquare.classList.add('taken');
+        gameBoard.displayMarks(gameBoard.gameArray);
+        displayWinner(gameBoard.checkWinner(gameBoard.gameArray));
+        player1.turn = true;
+        player2.turn = false;
+        name1.classList.add('turnShadow');
+        name2.classList.remove('turnShadow');
+      }
+  }
+
+  function displayWinner(result) {
+    let winScreen = document.querySelector('.winnerScreen');
+
+    if (result === "") {
+      return
+    } else if (result === player1.getMarker()) {
+      winScreen.textContent = "The winner is " + player1.getName() + "!";
+      winScreen.classList.remove('hidden');
+      name1.classList.add('turnShadow');
+      name2.classList.remove('turnShadow');
+    } else if (result === player2.getMarker()) {
+      winScreen.textContent = "The winner is " + player2.getName() + "!";
+      winScreen.classList.remove('hidden');
+      name1.classList.remove('turnShadow');
+      name2.classList.add('turnShadow');
+    } else if (result === "draw") {
+      winScreen.textContent = "The game ends in a DRAW";
+      winScreen.classList.remove('hidden');
+      name1.classList.remove('turnShadow');
+      name2.classList.remove('turnShadow');
+    }
+    gameSquares.forEach(square => {
+      square.removeEventListener('click', addMark);
+      square.classList.add('taken');
+    });
+  }
+
+  let resetBtn = document.querySelector('.restartGame');
+  resetBtn.addEventListener('click', resetGame);
+
+  let newGameBtn = document.querySelector('.newGame');
+  newGameBtn.addEventListener('click', startNewGame);
 
   function resetGame() {
     let winScreen = document.querySelector('.winnerScreen');
